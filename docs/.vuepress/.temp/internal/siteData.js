869a6c9495b8b2336ce92d0685c91ad78322d6ef
landon <1301807538@qq.com>
2026-05-22 09:44:50 +0800

    chore: update site base path and inject git metadata into temporary page files

--- docs/.vuepress/.temp/internal/siteData.js
+++ docs/.vuepress/.temp/internal/siteData.js
@@ -1,4 +1,4 @@
-export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"zh-CN\",\"title\":\"个人简历\",\"description\":\"\",\"head\":[],\"locales\":{}}")
+export const siteData = JSON.parse("{\"base\":\"/portfolio/\",\"lang\":\"zh-CN\",\"title\":\"个人简历\",\"description\":\"\",\"head\":[],\"locales\":{}}")
 
 if (import.meta.webpackHot) {
   import.meta.webpackHot.accept()
--- docs/.vuepress/.temp/pages/index.html.js
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -1,5 +1,5 @@
 import comp from "/Users/zhangxiaoqiang/Desktop/workspace/vuepress-starter/docs/.vuepress/.temp/pages/index.html.vue"
-const data = JSON.parse("{\"path\":\"/\",\"title\":\"\",\"lang\":\"zh-CN\",\"frontmatter\":{\"navbar\":true,\"sidebar\":false,\"contributors\":false,\"editLink\":false,\"lastUpdated\":false},\"headers\":[],\"git\":{},\"filePathRelative\":\"README.md\"}")
+const data = JSON.parse("{\"path\":\"/\",\"title\":\"\",\"lang\":\"zh-CN\",\"frontmatter\":{\"navbar\":true,\"sidebar\":false,\"contributors\":false,\"editLink\":false,\"lastUpdated\":false},\"headers\":[],\"git\":{\"updatedTime\":1779413367000,\"changelog\":[{\"hash\":\"fa0dbe42d9474992acdb520f763e736a029a7577\",\"time\":1779413367000,\"email\":\"1301807538@qq.com\",\"author\":\"landon\",\"message\":\"deploy: init portfolio\"}]},\"filePathRelative\":\"README.md\"}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
